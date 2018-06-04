4796d19ada650b30a9209bab92ced39072c49a53
TestAndunR <37790187+TestAndunR@users.noreply.github.com>
2018-06-04 11:31:25 +0530

    add an rds

---
+++ Validateisuue-332/ConnectionManager.js
@@ -0,0 +1,19 @@
+module.exports=function(){
+
+    this.dbConnections = [];
+
+    this.dbConnections["Test"] = {
+             host: process.env.EndPoint_rdsTest,
+             port: process.env.Port_rdsTest,
+             user: process.env.User_rdsTest,
+             password: process.env.Password_rdsTest,
+             database: "validation",
+        };
+    this.dbConnections["IssueValidator"] = {
+            host: process.env.EndPoint_rdsIssueValidator,
+            port: process.env.Port_rdsIssueValidator,
+            user: process.env.User_rdsIssueValidator,
+            password: process.env.Password_rdsIssueValidator,
+            database: "Validator",
+        };
+    };
--- Validateisuue-332/lambda.js
+++ Validateisuue-332/lambda.js
@@ -1,10 +1,29 @@
 let AWS = require('aws-sdk');
-let firebase = require('firebase');
-let connectionManager = require('./sigma334.js');
-let page = new Page();
-exports.handler = function(event, context, callback) {
+let connectionManager = require('./ConnectionManager');
+let SL_AWS = require('slappforge-sdk-aws');
+const rds = new SL_AWS.RDS(connectionManager);
+exports.handler = function (event, context, callback) {
 
-	console.log(page.name);	
+	// You can pass the existing connection to this function.
+	// A new connection will be created if it's not present as the third param 
+	// You must always end/destroy the DB connection after it's used
+	rds.query({
+		instanceIdentifier: 'IssueValidator',
+		query: "INSERT INTO('Issue')VALUES('sigma338')",
+		inserts: []
+	}, function (error, results, connection) {
+		if (error) {
+			console.log("Error occurred");
+			throw error;
+		} else {
+			console.log("Success")
+			console.log(results);
+		}
+
+		connection.end();
+	});
+
+
 	console.log("Validte sigma 334");
-	callback(null,'Successfully executed');
+	callback(null, 'Successfully executed');
 }
